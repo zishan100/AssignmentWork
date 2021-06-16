c2a4d7208f0d12d24863ca116641be3dcabf17f6
zishan100 <Zishanansari111@gmail.com>
2021-06-16 09:46:10 +0530

    Updates

---
+++ precache-manifest.90248e2bbda1be5f367c5d287f671772.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18ac8d44b5bddb546e09",
+    "url": "/AssignmentWork/static/css/main.17f715ee.chunk.css"
+  },
+  {
+    "revision": "18ac8d44b5bddb546e09",
+    "url": "/AssignmentWork/static/js/main.18ac8d44.chunk.js"
+  },
+  {
+    "revision": "54ce41f1b1fd540da083",
+    "url": "/AssignmentWork/static/js/1.54ce41f1.chunk.js"
+  },
+  {
+    "revision": "12cf12ff4bfc65a37873",
+    "url": "/AssignmentWork/static/js/runtime~main.12cf12ff.js"
+  },
+  {
+    "revision": "096020c4a0dfc4f8ad36d694cd81fcbc",
+    "url": "/AssignmentWork/index.html"
+  }
+];
